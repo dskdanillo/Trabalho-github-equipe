03e6527a4324fee5af775529e300997dbde8db38
Gustavocarvalho08 <gustavo_v_carvalho@estudante.sesisenai.org.br>
2025-09-24 19:29:37 -0300

    Adicionando Cep e ajustando regex

--- js/validacao.js
+++ js/validacao.js
@@ -1,31 +1,42 @@
 function validaFormulario(){
 
-    if(document.frmcadastro.txtnome.value==""){
+    if(document.getElementById('nome').value==""){
 
         alert("Preencha o campo Nome");
        document.frmcadastro.txtnome.focus();
        return false;
 
-    }else if(document.frmcadastro.txtnumero.value==""){
+    }else if(document.getElementById('numero').value==""){
 
-        alert("Preencha o campo Telefone");
+        alert("Preencha o campo numero");
             document.frmcadastro.txtnumero.focus();
             return false;
 
-    }else if(document.frmcadastro.txtcomplemento.value==""){
-            alert("Preencha o campo E-mail");
+    }else if(document.getElementById('cep').value==""){
+            alert("Preencha o campo Cep");
+            document.frmcadastro.txtcep.focus();
+            return false;
+        
+        }else if(document.getElementById('complemento').value==""){
+            alert("Preencha o campo complemento");
             document.frmcadastro.txtcomplemento.focus();
             return false;
+        
         }
 
         
         var nome = document.frmcadastro.txtnome.value;
-        var expRegNome = new RegExp("(?i)\brua\s+[a-zà-ú0-9\s\.]+");
+        var expRegNome = new RegExp("\\b(Rua|Avenida|Av|Travessa|Rodovia|Alameda|Estrada)\\s+([A-ZÁÉÍÓÚÂÊÔÃÕÇ][a-zA-ZáéíóúâêôãõçÁÉÍÓÚÂÊÔÃÕÇ\\s\\-]*)");
 
         if (!expRegNome.test(nome)){
         alert("Preencha o campo Nome da rua corretamente.");
         document.frmcadastro.txtnome.focus();
         return false;
         }
+
+        
+
+
+
     
 }
