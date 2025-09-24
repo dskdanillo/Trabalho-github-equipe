28858b3a218e1cd360f6e241b258abbb52b3b839
Gustavocarvalho08 <gustavo_v_carvalho@estudante.sesisenai.org.br>
2025-09-23 21:14:54 -0300

    Validações do js

--- js/validacao.js
+++ js/validacao.js
@@ -4,18 +4,20 @@ function validaFormulario(){
 
         alert("Preencha o campo Nome");
        document.frmcadastro.txtnome.focus();
+       return false;
 
     }else if(document.frmcadastro.txtnumero.value==""){
 
-        lert("Preencha o campo Telefone");
+        alert("Preencha o campo Telefone");
             document.frmcadastro.txtnumero.focus();
             return false;
 
-    }else if(document.frmcadastro.txtemail.value==""){
+    }else if(document.frmcadastro.txtcomplemento.value==""){
             alert("Preencha o campo E-mail");
             document.frmcadastro.txtcomplemento.focus();
             return false;
         }
 
+        
     
 }
